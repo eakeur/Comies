8e6fd017741b75c2414a011816e1c10798aee6dd
Igor Reis <igor.reisleandro@gmail.com>
2021-02-13 03:10:34 +0000

    mira su pasito

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -11,13 +11,8 @@ import 'controllers/products.controller.dart';
 void main(List<String> args) async {
   var parser = ArgParser()..addOption('port', abbr: 'p');
   var result = parser.parse(args);
-<<<<<<< HEAD
   //  result['port'] ?? Platform.environment['PORT'] ?? '8080'
   final server = Jaguar(port:8080);
-=======
-  // var port = int.parse(result['port']) ?? int.parse(Platform.environment['PORT']) ?? 8080;
-  final server = Jaguar(port: 8080);
->>>>>>> 272c1a10f1a9ccc07392afee537c8237c0ec9447
   server.add(reflect(ProductsController()));
   await server.serve();
   // collection = await DbLayer().connect(DBConnectionInfo(
